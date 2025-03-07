80248e5219d0016aba020640dd1a4ba0cabec12a
staszekscp <mariusz.stanisz@swmansion.com>
2025-03-07 15:32:00 +0100

    Add support to React Native 0.78

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,22 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    includeBuild("../node_modules/@react-native/gradle-plugin")
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         mavenLocal()
     }
 }
+
+plugins {
+    id("com.facebook.react.settings")
+}
+
+extensions.configure<com.facebook.react.ReactSettingsExtension> { autolinkLibrariesFromCommand() }
+includeBuild("../node_modules/@react-native/gradle-plugin")
 
 include(":app")
